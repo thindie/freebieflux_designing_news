9407b1dd0d06b094c09e8137237f7f1e96a07ce3
Alex <thindie1@mail.ru>
2023-07-20 10:23:41 +0200

    add icons and img

--- app/src/main/java/com/example/thindie/freebiefluxnewsappuikitimpl/ui/core/AppSearch.kt
+++ app/src/main/java/com/example/thindie/freebiefluxnewsappuikitimpl/ui/core/AppSearch.kt
@@ -5,17 +5,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import com.example.thindie.freebiefluxnewsappuikitimpl.ui.theme.commonShape
 
 @Composable
 fun AppSearch(state: InputAbleState) {
     OutlinedTextField(
-        value = state.currentLine.value, onValueChange = state::onTyping
+        shape = commonShape,
+        singleLine = true,
+        value = state.currentLine.value,
+        onValueChange = state::onTyping
     )
 }
 
 
 @Stable
-class InputAbleState() {
+class InputAbleState {
     private val _currentLine = mutableStateOf("")
     val currentLine: State<String>
         get() = _currentLine
